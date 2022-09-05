e54ed46827ce7408a2fbad6eda4a9ba10d579192
sdyoon94 <sdyoon94@gmail.com>
2022-09-05 12:04:04 +0900

    setting: 백엔드 스프링부트 프로젝트 생성(#94)

---
+++ Backend/src/main/java/com/a202/guirini/GuiriniApplication.java
@@ -0,0 +1,13 @@
+package com.a202.guirini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiriniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuiriniApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/com/a202/guirini/GuiriniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a202.guirini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuiriniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
