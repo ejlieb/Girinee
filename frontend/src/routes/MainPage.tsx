595f9866817b2420103773a8f4e06b27d37f35ab
ejlieb <coryonronda@gmail.com>
2022-09-13 13:29:28 +0900

    feature-front-threeloaded

--- frontend/src/routes/MainPage.tsx
+++ frontend/src/routes/MainPage.tsx
@@ -1,10 +1,24 @@
+import * as THREE from 'three'
+import { createRoot } from 'react-dom/client'
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
+import { Box } from '../components/mainpage/three'
+import { Model } from '../components/mainpage/Thr10'
+import { Test } from '../components/mainpage/Test'
+
+
 export function MainPage() {
   // script
 
   // JSX
   return (
     <div>
-      <h1>MainPage</h1>
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Test/>
+    </Canvas>,
     </div>
   )
 }
