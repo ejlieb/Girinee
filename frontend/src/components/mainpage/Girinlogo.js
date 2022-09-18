21d4c30729ccba75e38c74af21dec0f5704d6e68
오채명 <dhcoaud98@naver.com>
2022-09-19 08:49:54 +0900

    feat: 로그인 시도

--- frontend/src/components/mainpage/Girinlogo.js
+++ frontend/src/components/mainpage/Girinlogo.js
@@ -34,7 +34,7 @@ export function Logo(props) {
   let [intensity, setIntensity] = useState(0)
 
   let timer = useInterval(() => {
-    if (localStorage.getItem('token') != null) {
+    if (localStorage.getItem('accessToken') != null) {
       setIntensity(intensity + 0.01)
       clearInterval(timer)
       console.log('hi' + intensity)}
