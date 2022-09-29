3d62fa39af8b8569bf4eb4a00f769db42590a2cb
SSAFY <coryonronda@gmail.com>
2022-09-29 10:19:47 +0900

    feature-frontend-threeEddChords

--- frontend/src/components/chordtable/C.js
+++ frontend/src/components/chordtable/C.js
@@ -9,10 +9,16 @@ export function C(props) {
   const { nodes, materials } = useGLTF('/C.glb')
   return (
     <group {...props} dispose={null}>
-      <mesh geometry={nodes.mesh_0.geometry} material={nodes.mesh_0.material} >
-      <meshStandardMaterial attach="material" emissive={'lightblue'} emissiveIntensity={0.8} color="lightblue" roughness={1} metalness={0.3} />
+      <mesh geometry={nodes.mesh_1.geometry} material={nodes.mesh_1.material}>
+      <meshStandardMaterial attach="material" emissive={'lightblue'} emissiveIntensity={1} color="lightblue" roughness={1} metalness={0.3} />
       </mesh>
-
+      <mesh geometry={nodes.mesh_2.geometry} material={nodes.mesh_2.material}>
+      <meshStandardMaterial attach="material" emissive={'lightblue'} emissiveIntensity={1} color="lightblue" roughness={1} metalness={0.3} />
+      </mesh>
+      <mesh geometry={nodes.mesh_3.geometry} material={nodes.mesh_3.material} />
+      <mesh geometry={nodes.mesh_4.geometry} material={nodes.mesh_4.material} />
+      <mesh geometry={nodes.mesh_5.geometry} material={nodes.mesh_5.material} />
+      <mesh geometry={nodes.mesh_6.geometry} material={nodes.mesh_6.material} />
     </group>
   )
 }
