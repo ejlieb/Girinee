fd7a8784dbc6f7ff249e33052e0f85d70bbecf9b
sdyoon94 <sdyoon94@gmail.com>
2022-09-29 09:24:03 +0900

    feat: 유저기능 수정

--- Backend/src/main/java/com/a202/girinee/controller/UserController.java
+++ Backend/src/main/java/com/a202/girinee/controller/UserController.java
@@ -1,9 +1,9 @@
 package com.a202.girinee.controller;
 
 
-import com.a202.girinee.entity.User;
-import com.a202.girinee.repository.UserRepository;
+import com.a202.girinee.dto.response.UserResponseDto;
 import com.a202.girinee.security.CustomUserDetails;
+import com.a202.girinee.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -15,14 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
 @Log4j2
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/user")
+@RequestMapping("/api/user")
 public class UserController {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
 
     @GetMapping("/me")
     @PreAuthorize("hasRole('USER')")
-    public User getCurrentUser(@AuthenticationPrincipal CustomUserDetails user) {
-        return userRepository.findById(user.getId()).orElseThrow(() -> new IllegalStateException("not found user"));
+    public UserResponseDto getUserInfo(@AuthenticationPrincipal CustomUserDetails user) {
+        return userService.findUserInfo(user.getId());
+    }
+
+    @GetMapping("/logout")
+    @PreAuthorize("hasRole('USER')")
+    public void logout(@AuthenticationPrincipal CustomUserDetails user) {
+        userService.logout(user.getId());
     }
 }
