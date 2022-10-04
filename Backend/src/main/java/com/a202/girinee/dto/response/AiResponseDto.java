93052b659e856c272cb6d482fd9888db4c55fe26
sdyoon94 <sdyoon94@gmail.com>
2022-10-04 17:48:22 +0900

    feat: 스프링부트 서버에서 인공지능 API 호출 테스트 4

--- Backend/src/main/java/com/a202/girinee/dto/response/AiResponseDto.java
+++ Backend/src/main/java/com/a202/girinee/dto/response/AiResponseDto.java
@@ -1,5 +1,6 @@
 package com.a202.girinee.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +9,7 @@ import java.io.Serializable;
 @Data
 @NoArgsConstructor
 public class AiResponseDto implements Serializable {
+    @JsonProperty("is_correct")
     private Boolean isCorrect;
     private Integer score;
 }
