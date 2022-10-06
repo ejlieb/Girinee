85b2cea6bda6ff7ed215322af57ef1cc663ce9ab
sdyoon94 <sdyoon94@gmail.com>
2022-10-06 11:37:56 +0900

    fix: 인공지능 서버 버그 수정

--- Backend-python/models.py
+++ Backend-python/models.py
@@ -63,12 +63,5 @@ def predict_hard(model_list,img_paths):
         data[0] = normalized_image_array
         predictions.append(model_list[i].predict(data))
     prediction = predictions[0]+predictions[1]+ 0.5*predictions[2]
-    max = np.argmax(prediction[0])
-    print(prediction)
-    print(max)
-    score = f'{max*40:d}'
-    print(score)
-    return chords[max], score
-
-
-
+    score = f'{max(prediction[0])*40:d}'
+    return chords[np.argmax(prediction[0])], score
